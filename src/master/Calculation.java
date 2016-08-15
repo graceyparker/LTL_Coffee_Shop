85cf29e9b8de82df0479625a213ae428b6c44115
graceyparker <laurinjparker@gmail.com>
2016-08-15 13:29:27 -0400

    First commit

---
+++ src/master/Calculation.java
@@ -0,0 +1,10 @@
+package master;
+
+public class Calculation {
+	
+	double itemPrice = 0;
+	double quantity = 0;
+	
+	public double 
+
+}
---
+++ src/master/Product.java
@@ -0,0 +1,21 @@
+package master;
+
+public class Product {
+public static void main(String[] args) {
+		
+
+		int[] ar = {1,2,3,4};
+		
+		int i=0;
+		int Max = ar[0];
+				
+		while (i<ar.length)
+				{
+					if (ar[i]>Max) {
+						Max = ar[i]; }
+					i++;
+				}
+}
+}
+	
+
